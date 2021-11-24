66450894082eca546acb5e8472f156a33d64fc75
sernadesigns <sernadesigns@gmail.com>
2021-11-24 08:01:28 -0700

    Adds post mutation feature

--- src/schema.ts
+++ src/schema.ts
@@ -1,27 +1,47 @@
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import typeDefs from './schema.graphql';
 
+// type definitions
 type Link = {
   id: string;
   url: string;
   description: string;
 }
 
+// in lieau of database
 const links: Link[] = [{
   id: 'link-0',
   url: 'www.howtographql.com',
   description: 'Fullstack tutorial for GraphQL'
 }]
 
+// resolver functions
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
     feed: () => links,
   },
+  // trivial resolver def and not needed
   Link: {
     id: (parent: Link) => parent.id,
     description: (parent: Link) => parent.description,
     url: (parent: Link) => parent.url,
+  },
+  Mutation: {
+    post: (parent: unknown, args: { description: string, url: string }): Link => {
+      let idCount = links.length;
+      const link: Link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+
+      // pushes link to collection
+      links.push(link);
+
+      // returns one link
+      return link;
+    }
   }
 }
 
