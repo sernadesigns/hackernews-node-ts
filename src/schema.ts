44501f39caac5471eeb82e469a487466b89f4d01
sernadesigns <sernadesigns@gmail.com>
2021-11-29 02:13:47 -0700

    Adds sorting

--- src/schema.ts
+++ src/schema.ts
@@ -1,7 +1,7 @@
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import { GraphQLContext } from './context';
 import typeDefs from './schema.graphql';
-import { Link, User } from '@prisma/client';
+import { Link, User, Prisma } from '@prisma/client';
 import { APP_SECRET } from './auth';
 import { compare, hash } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
@@ -11,8 +11,41 @@ import { PubSubChannels } from './pubsub';
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: async (parent: unknown, args: {}, context: GraphQLContext) =>
-      context.prisma.link.findMany(),
+    feed: async (
+      parent: unknown,
+      args: {
+        filter?: string;
+        skip?: number;
+        take?: number;
+        orderBy?: {
+          description?: Prisma.SortOrder;
+          url?: Prisma.SortOrder;
+          createdAt?: Prisma.SortOrder;
+        }
+      },
+      context: GraphQLContext
+    ) => {
+      const where = args.filter
+        ? {
+            OR: [
+              { description: { contains: args.filter } },
+              { url: { contains: args.filter } }
+            ]
+          }
+        : {};
+      
+      const totalCount = await context.prisma.link.count({ where });
+      const links = await context.prisma.link.findMany({
+        where,
+        skip: args.skip,
+        take: args.take,
+        orderBy: args.orderBy,
+      });
+      return {
+        count: totalCount,
+        links,
+      };
+    },
     me: (parent: unknown, args: {}, context: GraphQLContext) => {
       if (context.currentUser === null) {
         throw new Error('Unauthenticated!');
